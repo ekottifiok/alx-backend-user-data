4edfc31555feffd8174339e604ca99f54c443b88
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-03 05:53:39 +0100

    update on auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,7 +2,7 @@
 """Auth Module that carries the Auth class and it functions
 """
 from typing import List, TypeVar
-
+from re import search
 
 class Auth:
     """Auth model to manage API authentication
@@ -24,8 +24,15 @@ class Auth:
             return True
         excluded_paths = [item if item[-1] != '/' else item[:-1] for item in excluded_paths]
         path = path if path[-1] != '/' else path[:-1]
+        for p in excluded_paths:
+            if "*" in p:
+                if search(p, path):
+                    return False
+            if p == path:
+                return False
+        return True
+                
         
-        return True if path not in excluded_paths else False
     
     def authorization_header(self, request=None) -> str:
         """public method def authorization_header(self, request=None) -> str:
