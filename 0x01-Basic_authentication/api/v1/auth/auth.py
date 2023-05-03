ee3f40a643eab0a9e13a156a2cd083a178252295
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-03 06:02:22 +0100

    h

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,11 +4,11 @@
 from typing import List, TypeVar
 from re import search
 
+
 class Auth:
     """Auth model to manage API authentication
     """
-           
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method def require_auth(self, path: str, excluded_paths:
         List[str]) -> bool:
@@ -22,7 +22,8 @@ class Auth:
         """
         if path is None or excluded_paths is None:
             return True
-        excluded_paths = [item if item[-1] != '/' else item[:-1] for item in excluded_paths]
+        excluded_paths = [item if item[-1] != '/' else item[:-1]
+                          for item in excluded_paths]
         path = path if path[-1] != '/' else path[:-1]
         for p in excluded_paths:
             if "*" in p:
@@ -31,9 +32,7 @@ class Auth:
             if p == path:
                 return False
         return True
-                
-        
-    
+
     def authorization_header(self, request=None) -> str:
         """public method def authorization_header(self, request=None) -> str:
 
@@ -43,10 +42,10 @@ class Auth:
         Returns:
             str: _description_
         """
-        if request is None or not isinstance(request, dict) or 'Authorization' not in request.keys():
+        if request is None or not isinstance(request, dict) or \
+            'Authorization' not in request.keys():
             return None
-        return request['Authorization'] 
-        
-    
+        return request['Authorization']
+
     def current_user(self, request=None) -> TypeVar('User'):
         return None
